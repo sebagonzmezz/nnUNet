c20e2e87792f9ab1c8c58d7060374e80c1181b7a
sebagonzmezz <sebagonzalez@ug.uchile.cl>
2025-12-05 20:52:40 +0000

    refactor trainer classes and add dataset files

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainerCustom/nnUNetTrainerCustom.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainerCustom/nnUNetTrainerCustom.py
@@ -1,8 +1,6 @@
 from typing import Tuple, Union, List
 
-from dynamic_network_architectures.architectures.unet import PlainConvUNet
 from torch import nn
-import torch
 
 from nnunetv2.utilities.get_network_from_plans import get_network_from_plans
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
@@ -17,23 +15,14 @@ class G2_in(nnUNetTrainer):
                                    num_output_channels: int,
                                    enable_deep_supervision: bool = True,
                                    pretrained_encoder = None) -> nn.Module:
-        model = PlainConvUNet(
-            input_channels=1,
-            n_stages=6,
-            features_per_stage=[1, 48, 96, 192, 384, 384],
-            conv_op=torch.nn.modules.conv.Conv3d,
-            kernel_sizes=3,
-            strides=[1, 4, 2, 2, 2, 1],
-            n_conv_per_stage=2,
-            num_classes=2,
-            n_conv_per_stage_decoder=2,
-            conv_bias=True,
-            norm_op=torch.nn.modules.instancenorm.InstanceNorm3d,
-            norm_op_kwargs={"eps": 1e-05,"affine": True},
-            nonlin=torch.nn.LeakyReLU,
-            nonlin_kwargs={"inplace": True},
-            deep_supervision=enable_deep_supervision,
-        )
+        model = get_network_from_plans(
+            architecture_class_name,
+            arch_init_kwargs,
+            arch_init_kwargs_req_import,
+            num_input_channels,
+            num_output_channels,
+            allow_init=True,
+            deep_supervision=enable_deep_supervision)
         encoder = SwinTransformerV2(
             in_chans=1,
             embed_dim=48,
@@ -70,23 +59,14 @@ class G2_enc(nnUNetTrainer):
                                    num_output_channels: int,
                                    enable_deep_supervision: bool = True,
                                    pretrained_encoder = None) -> nn.Module:
-        model = PlainConvUNet(
-            input_channels=1,
-            n_stages=6,
-            features_per_stage=[24, 48, 96, 192, 384, 384],
-            conv_op=torch.nn.modules.conv.Conv3d,
-            kernel_sizes=3,
-            strides=[1, 4, 2, 2, 2, 1],
-            n_conv_per_stage=2,
-            num_classes=2,
-            n_conv_per_stage_decoder=2,
-            conv_bias=True,
-            norm_op=torch.nn.modules.instancenorm.InstanceNorm3d,
-            norm_op_kwargs={"eps": 1e-05,"affine": True},
-            nonlin=torch.nn.LeakyReLU,
-            nonlin_kwargs={"inplace": True},
-            deep_supervision=enable_deep_supervision,
-        )
+        model = get_network_from_plans(
+            architecture_class_name,
+            arch_init_kwargs,
+            arch_init_kwargs_req_import,
+            num_input_channels,
+            num_output_channels,
+            allow_init=True,
+            deep_supervision=enable_deep_supervision)
         encoder = SwinTransformerV3(
             conv_output_channels=24,
             in_chans=1,
@@ -115,7 +95,7 @@ class G2_enc(nnUNetTrainer):
         print(f"Trainable parameters in encoder: {trainable_params:,}")
         return model
 
-class G2_up(nnUNetTrainer):
+class G2_up_all(nnUNetTrainer):
     @staticmethod
     def build_network_architecture(architecture_class_name: str,
                                    arch_init_kwargs: dict,
@@ -124,23 +104,14 @@ class G2_up(nnUNetTrainer):
                                    num_output_channels: int,
                                    enable_deep_supervision: bool = True,
                                    pretrained_encoder = None) -> nn.Module:
-        model = PlainConvUNet(
-            input_channels=1,
-            n_stages=5,
-            features_per_stage=[48, 96, 192, 384, 384],
-            conv_op=torch.nn.modules.conv.Conv3d,
-            kernel_sizes=3,
-            strides=[1, 4, 2, 2, 2],
-            n_conv_per_stage=2,
-            num_classes=2,
-            n_conv_per_stage_decoder=2,
-            conv_bias=True,
-            norm_op=torch.nn.modules.instancenorm.InstanceNorm3d,
-            norm_op_kwargs={"eps": 1e-05,"affine": True},
-            nonlin=torch.nn.LeakyReLU,
-            nonlin_kwargs={"inplace": True},
-            deep_supervision=enable_deep_supervision,
-        )
+        model = get_network_from_plans(
+            architecture_class_name,
+            arch_init_kwargs,
+            arch_init_kwargs_req_import,
+            num_input_channels,
+            num_output_channels,
+            allow_init=True,
+            deep_supervision=enable_deep_supervision)
         encoder = SwinTransformerV4(
             in_chans=1,
             embed_dim=48,
